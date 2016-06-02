266dcedc04c69cb9e4019f0323dd9d55e047f9ef
Dmytro Kryvenko <dkryvenk@cisco.com>
2016-06-02 15:38:37 +0300

    Add package.html to Adapter

--- adapter/src/main/java/ua/cn/dmitrykrivenko/adapter/EnumerationIteratorAdapter.java
+++ adapter/src/main/java/ua/cn/dmitrykrivenko/adapter/EnumerationIteratorAdapter.java
@@ -9,21 +9,24 @@ import java.util.Iterator;
  */
 public class EnumerationIteratorAdapter implements Iterator {
 
-    private Enumeration enumeration;
+    private final Enumeration enumeration;
 
     public EnumerationIteratorAdapter(Enumeration enumeration) {
         this.enumeration = enumeration;
     }
 
+	@Override
     public boolean hasNext() {
         return enumeration.hasMoreElements();
 
     }
 
+	@Override
     public Object next() {
         return enumeration.nextElement();
     }
 
+	@Override
     public void remove() {
         throw new UnsupportedOperationException("Not supported");
     }
